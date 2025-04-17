c3e1a1bf8dd2a22d992071573cd93be6bf1d791a
jame <154039752+jameyangzy@users.noreply.github.com>
2025-04-17 17:17:30 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js';
  
 // Supabase configuration
  const supabaseUrl = 'https://febzoufkcsvpkbjvkeij.supabase.co';
- const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlYnpvdWZrY3N2cGtianZrZWlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4MDY5MTIsImV4cCI6MjA1ODM4MjkxMn0.ox1vLEy9qjMhxJsQa9f81Hm9DKJWQWqhbiMEReqmMLU';
+ const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlYnpvdWZrY3N2cGtianZrZWlqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MjgwNjkxMiwiZXhwIjoyMDU4MzgyOTEyfQ.9mzChoVrEMJEYmXSBbgkAKUE_zEEixPon3dPMjrJmSs';
  
  const supabase = createClient(supabaseUrl, supabaseKey);
  
